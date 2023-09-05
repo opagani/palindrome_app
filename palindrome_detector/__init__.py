f557c0300ef907065df33e3600c20e26b1e4e210
opagani <paganio@yahoo.com>
2023-09-04 21:53:57 -0700

    Added templates directory and hrml files

--- palindrome_detector/__init__.py
+++ palindrome_detector/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app(test_config=None):
   """Create and configure the app."""
@@ -21,7 +21,15 @@ def create_app(test_config=None):
   
   @app.route("/")
   def index():
-    return "hello, world!"
+    return render_template("index.html")
+  
+  @app.route("/about")
+  def about():
+    return render_template("about.html")
+  
+  @app.route("/palindrome")
+  def palindrome():
+    return render_template("palindrome.html")
   
   return app
 
